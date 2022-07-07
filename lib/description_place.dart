f67d64dd4f291d1f03a9819477f1d9e1f5ff9cf4
Kevin Cordova <kevin.cordova@bayteq.com>
2022-07-06 23:51:36 -0500

    Creation project

---
+++ lib/description_place.dart
@@ -0,0 +1,80 @@
+import 'package:flutter/material.dart';
+
+class DescriptionPlace extends StatelessWidget {
+  late String namePlaceText, descriptionPlace;
+  late int starts;
+
+  DescriptionPlace(this.namePlaceText, this.descriptionPlace, this.starts,
+      {Key? key})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final startBorder = Container(
+      margin: const EdgeInsets.only(
+        top: 323.0,
+        right: 3.0,
+      ),
+      child: const Icon(
+        Icons.star_border,
+        color: Color(0xFFf2C611),
+      ),
+    );
+
+    final startHalf = Container(
+      margin: const EdgeInsets.only(
+        top: 323.0,
+        right: 3.0,
+      ),
+      child: const Icon(
+        Icons.star_half,
+        color: Color(0xFFf2C611),
+      ),
+    );
+    final start = Container(
+      margin: const EdgeInsets.only(
+        top: 323.0,
+        right: 3.0,
+      ),
+      child: const Icon(
+        Icons.star,
+        color: Color(0xFFf2C611),
+      ),
+    );
+
+    final titleStart = Row(
+      children: [
+        Container(
+          margin: const EdgeInsets.only(
+              top: 320.0, // Height between toolbar and title
+              left: 20.0,
+              right: 20.0),
+          child: Text(
+            namePlaceText,
+            style: const TextStyle(
+                fontSize: 30.0,
+                fontWeight: FontWeight.bold,
+                fontFamily: "Lato"),
+            textAlign: TextAlign.justify,
+          ),
+        ),
+        // Starts
+        Row(
+          children: [start, start, start, start, startHalf],
+        ),
+      ],
+    );
+    final textDescription = Container(
+      margin: const EdgeInsets.only(top: 24.0, left: 20.0, right: 20.0),
+      child: Text(
+        descriptionPlace,
+        style: const TextStyle(fontSize: 16.0, fontFamily: "Lato"),
+        textAlign: TextAlign.justify,
+      ),
+    );
+
+    return Column(
+      children: [titleStart, textDescription],
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import 'package:platzi_tripss_app/description_place.dart';
+
+///
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // Try running your application with "flutter run". You'll see the
+        // application has a blue toolbar. Then, without quitting the app, try
+        // changing the primarySwatch below to Colors.green and then invoke
+        // "hot reload" (press "r" in the console where you ran "flutter run",
+        // or simply save your changes to "hot reload" in a Flutter IDE).
+        // Notice that the counter didn't reset back to zero; the application
+        // is not restarted.
+        primarySwatch: Colors.blue,
+      ),
+      //home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: Scaffold(
+          appBar: AppBar(
+            title: const Text("Share"),
+            leading: const IconButton(
+              icon: Icon(Icons.arrow_back),
+              onPressed: null,
+            ),
+            centerTitle: false,
+          ),
+          body: DescriptionPlace(
+              "Lorem ipsum",
+              "Lorem ipsum odor amet, consectetuer adipiscing elit. Lectus felis penatibus. Taciti nam at nam. Vulputate rutrum mollis. Quam orci magna dictumst.",
+              5)),
+    );
+  }
+}
