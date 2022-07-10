eca0123f9b89d829026593056d6b075b096d1a5d
kevc77 <kcordovas77@gmail.com>
2022-07-10 13:14:46 -0500

    Change folder myApp in widget_test.dart

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,8 +7,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
-import 'package:platzi_tripss_app/main.dart';
+import 'package:platzi_tripss_app/my_app.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
